1811330cc95bff6a68724f76e43a25c6de382c74
Samindi Sankalpani <155771316+SamindiS@users.noreply.github.com>
2026-01-04 16:31:31 +0530

    resolve errors and run

--- android/app/src/main/java/com/example/my_autism_app/MainActivity.kt
+++ android/app/src/main/java/com/example/my_autism_app/MainActivity.kt
@@ -3,6 +3,5 @@ package com.example.my_autism_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // Android 10 (API 29) compatible
-    // MultiDex support is handled in build.gradle
 }
+
